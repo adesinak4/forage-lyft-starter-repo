132587c25c7274797f74b8e51b1225b689edba62
Oluwakayode Adesina <adesinak4@gmail.com>
2023-04-27 16:23:27 +0100

    Edit

--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -2,8 +2,10 @@ from datetime import datetime
 
 from engine.willoughby_engine import WilloughbyEngine
 
+from battery.nubbin_battery import NubbinBattery
 
-class Rorschach(WilloughbyEngine):
+
+class Rorschach(WilloughbyEngine, NubbinBattery):
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
         if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
